5999cae414fc56c4b92aaea92db89503c78bc92e
Akari <venerosandres10@gmail.com>
2026-01-11 13:20:44 -0600

    delete innecesary functions of ExportRepository

--- app/src/main/java/com/habitiora/linkarium/data/repository/ExportRepository.kt
+++ app/src/main/java/com/habitiora/linkarium/data/repository/ExportRepository.kt
@@ -1,15 +1,7 @@
 package com.habitiora.linkarium.data.repository
 
 import com.habitiora.linkarium.core.exporters.ExportRequest
-import java.io.OutputStream
 
 interface ExportRepository {
     suspend fun export(request: ExportRequest)
-    suspend fun exportGardensJson(output: OutputStream): Result<Unit>
-    suspend fun exportGardensTxt(output: OutputStream): Result<Unit>
-    suspend fun exportGardensPdf(output: OutputStream): Result<Unit>
-
-    suspend fun exportGardenJson(gardenId: Long, output: OutputStream): Result<Unit>
-    suspend fun exportGardenTxt(gardenId: Long, output: OutputStream): Result<Unit>
-    suspend fun exportGardenPdf(gardenId: Long, output: OutputStream): Result<Unit>
 }
