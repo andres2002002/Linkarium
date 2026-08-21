d4c882a4f6ccae71f4a75b8d549a34800b0e280f
Andres Veneros <dev@veneros.dev>
2026-08-21 13:30:51 -0600

    chore: bump version to 1.0.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "com.habitiora.linkarium"
         minSdk = 28
         targetSdk = 37
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
